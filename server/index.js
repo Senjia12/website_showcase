61c27cb5784e478a1796c99b29054b8ef9c92e51
Senjia12 <senjiacorp@gmail.com>
2026-08-11 23:07:43 +0200

    env and json run scripts fix

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ import dotenv from "dotenv";
 import connectDB from "./config/database.js";
 import app from "./app.js";
 
+// for potential test suite i may add later
+const envFile = process.env.NODE_ENV === 'test' ? '.env.test' : '.env';
+
 dotenv.config({
   path: '.env.test',
   override: true
@@ -24,5 +27,7 @@ const startServer = async () => {
     console.log("MongoDB connection failed", error);
   }
 }
+console.log('Loaded env file:', envFile);
+console.log('Mongo URI:', process.env.MONGODB_URI);
 
 startServer();
